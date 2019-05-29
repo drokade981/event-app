3372f7bdead459c7ec54e0541eabc269f7188986
Devendra Rokade <44425686+drokade981@users.noreply.github.com>
2019-05-29 10:58:13 +0530

    Create api_lang.php

---
+++ application/language/english/api_lang.php
@@ -0,0 +1,47 @@
+<?php
+$lang['login_error'] = "Email or password is wrong. Please try again";
+$lang['auth_error'] = "You are not authorised to login.";
+$lang['email_exist'] = "This email address already exists.";
+$lang['mobile_exist'] = "This mobile number already exists.";
+$lang['unauthorize_access'] = "You are not authorised to access";
+$lang['email_password_reset'] = "An email for password reset has been sent to your email address. Please check";
+$lang['user_blocked'] = "Your account has been blocked.";
+$lang['user_fetched'] = "User details fetched successfully.";
+$lang['wront_otp'] = "Please enter correct OTP.";
+$lang['wrong_email_mobile'] = "Email or mobile does not exist. Please enter registered email or password";
+$lang['wrong_mobile'] = "Mobile number does not exist. Please enter registered mobile number";
+$lang['otp_sent_to_mobile'] = "OTP successfully sent to your mobile";
+$lang['otp_sent_to_mobile_email'] = "OTP successfully sent to your mobile number and email";
+
+$lang['no_data'] = "No data found";
+$lang['error'] = "Some error occucred.Please try again";
+$lang['registration_succ'] =  "You are successfully registered.";
+
+$lang['machine_add_succ'] = "Machine added successfully";
+$lang['machine_add_draft_succ'] = "Machine added as draft successfully";
+$lang['machine_update_succ'] = "Machine updated successfully";
+$lang['machine_fetched'] = "Machine detail fetched successfully";
+$lang['favourite_added'] = "Machine added to favourite.";
+
+
+$lang['key_required'] =  "Please provide ";
+$lang['profile_update_success'] =  "Profile updated successfully";
+
+$lang['notification_success'] =  "Notification fetched successfully";
+$lang['no_notification'] =  "No Notification Found";
+
+$lang['feedback_success'] =  "Feedback fetched successfully";
+$lang['no_feedback'] =  "No feeback Found";
+
+$lang['old_pass_not_match'] =  "Old password doesn't match";
+$lang['password_change_success'] = "Password changed successfully.";
+
+
+$lang['price_quoted'] = "Price quoted successfully.";
+$lang['ad_renewed'] = "Ad renewed successfully";
+$lang['removed_favourite'] = "Machine removed from favourite list successfully";
+$lang['marked_as_sold'] = "Machine marked as sold successfully";
+
+$lang['coupon_applied'] = "Coupon applied successfully";
+$lang['wrong_coupon'] = "Wrong coupon applied";
+$lang['category_interest_added'] = "Your category interest saved successfully";
