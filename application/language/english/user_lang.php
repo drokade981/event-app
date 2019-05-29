34c99d2e0ca013480223b7204d0e7a81af7fd03d
Devendra Rokade <44425686+drokade981@users.noreply.github.com>
2019-05-29 10:58:40 +0530

    Create user_lang.php

---
+++ application/language/english/user_lang.php
@@ -0,0 +1,33 @@
+<?php
+$lang['login_error'] = "Email or password is wrong. Please try again";
+$lang['auth_error'] = "Your email has not registered as attendee";
+$lang['error'] = "Some error occucred.Please try again";
+$lang['update_category_succ'] = "Category updated successfully";
+
+
+$lang['old_password_wrong'] = "Current password is wrong";
+$lang['password_update_success'] = "Password updated successfully";
+$lang['password_reset_success'] = "Password reset successfully";
+
+$lang['email_password_reset_send'] = "Password reset link is successfully sent to your email.";
+$lang['password_change_success'] = "Password changed successfully.";
+$lang['email_wrong_error'] = "Please enter email associated to admin login";
+
+$lang['desc_req'] = "Description required";
+$lang['added_succ'] = " added successfully";
+$lang['update_succ'] = " updated successfully";
+
+$lang['profile_update_success'] = "Profile updated successfully";
+
+$lang['duplicate_plan'] = "This plan name is already exist";
+$lang['duplicate_mobile'] = "This mobile number is already exist";
+$lang['duplicate_coupon_code'] = "This coupon code is already exist";
+$lang['duplicate_coupon_title'] = "This coupon title is already exist";
+
+
+
+$lang['user_blocked'] = "Your account is blocked";
+$lang['user_deleted'] = "Your account is deleted";
+
+$lang['otp_mismatch'] = "Wrong otp entered";
+$lang['link_expired'] = "This link is expired.Please try again";
